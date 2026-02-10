973e9b76893c7c905a95f8e63d2c3de0dc1d5ae6
unknown <{texnobola2@gmail.com}>
2026-02-11 00:29:25 +0500

    fixes3

--- src/hooks/useRealtimeGame.js
+++ src/hooks/useRealtimeGame.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../supabaseClient';
 
 export const useRealtimeGame = (roomId) => {
   const [room, setRoom] = useState(null);
